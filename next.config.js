5879770c69c253e466872472aad3809ea07b124d
Fabien Morrow <fabien.morrow@protonmail.com>
2023-03-25 19:15:53 +0100

    added sharp

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   experimental: {
     appDir: false,
-    fontLoaders: [
-      { loader: "@next/font/google", options: { subsets: ["latin"] } },
-    ],
+    // fontLoaders: [
+    //   { loader: "@next/font/google", options: { subsets: ["latin"] } },
+    // ],
   },
 };
 
